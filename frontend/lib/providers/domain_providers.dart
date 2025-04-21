f6217b488f25203b637b33b2f76ba279a11b3219
yuukitakahashiag <yuuki.takahashi.ag@nttqonoq.com>
2025-04-21 23:46:24 +0900

    add : provider

--- frontend/lib/providers/domain_providers.dart
+++ frontend/lib/providers/domain_providers.dart
@@ -1,9 +1,23 @@
 // SearchBooks Usecaseの窓口(Provider)を作成
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend/domain/usecases/get_books_by_status.dart';
 import 'package:frontend/domain/usecases/search_books.dart';
+import 'package:frontend/domain/usecases/update_book_status.dart';
+import 'package:frontend/providers/data_providers.dart';
 
+// SearchBooks Usecaseの窓口(Provider)を作成
 final searchBooksUseCaseProvider = Provider<SearchBooks>((ref) {
   return SearchBooks(ref);
 });
 
+// GetBooksByStatus Usecaseの窓口(Provider)を作成
+final getBooksByStatusUseCaseProvider = Provider<GetBooksByStatus>((ref){
+  final repository = ref.watch(bookRepositoryProvider);
+  return GetBooksByStatus(repository);
+});
 
+// UpdateBookStatus Usecaseの窓口(Provider)を作成
+final updateBookStatusProvider = Provider<UpdateBookStatus>((ref){
+  final repository = ref.watch(bookRepositoryProvider);
+  return UpdateBookStatus(repository);
+});
